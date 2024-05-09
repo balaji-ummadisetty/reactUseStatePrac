bbaa1f905ceb962ca36c4c480dfaecc019e68351
balaji-ummadisetty <balaji.ummadisettyb@gmail.com>
2024-05-09 17:01:28 +0530

    usestate

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './App.css';
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [Isopen, setIsopen] = useState(true);
+  function Update() {
+    if (step < 3) setStep((step) => step+1);
+  }
+  function decrease() {
+    if (step > 1) setStep((step) => step-1);
+  }
+  return (
+    <div>
+      <button className='close' onClick={()=>setIsopen((Isopen) => !Isopen)}>&times;</button>
+      {Isopen &&(
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 && "active"}`}>1</div>
+            <div className={`${step >= 2 && "active"}`}>2</div>
+            <div className={`${step >= 3 && "active"}`}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button className="previous" style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={decrease}>Previous</button>
+            <button className="next" style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={Update}>Next</button>
+          </div>
+        </div>
+      )
+        }
+    </div>
+  
+  )
+}
+
+export default App
